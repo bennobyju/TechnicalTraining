83c6abc2f09b46e051a868b1ab9a306b51fdbdf4
bennobyju <bennobyju@gmail.com>
2025-03-07 12:45:18 +0530

    Create triangle.py

---
+++ triangle.py
@@ -0,0 +1,10 @@
+import math
+a=float(input())
+b=float(input())
+c=float(input())
+if(a+b>c and b+c>a and a+c>b):
+    s = (a+b+c)/2
+    area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+    print('The area of the triangle is: {:.2f}'.format(area))
+else:
+    print('Invalid Triangle')
